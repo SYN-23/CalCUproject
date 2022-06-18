7c5795c5a2053cafbbeb41ae4648f9e268d6687f
SYN-23 <sayandas12375@gmail.com>
2022-06-19 02:43:22 +0530

    Updated on 19/06/2022

--- app/src/main/java/com/apk/calcu/MainActivity.java
+++ app/src/main/java/com/apk/calcu/MainActivity.java
@@ -23,13 +23,13 @@ public class MainActivity extends AppCompatActivity {
         workingsView = (TextView) findViewById(R.id.workingTextView);
         resultsView = (TextView) findViewById(R.id.resultTextView);
     }
-    private void setWorkings(String givenvalue){
-        workings = workings + givenvalue;
+    private void setWorkings(String givenValue){
+        workings = workings + givenValue;
         workingsView.setText(workings);
     }
 
     public void clearOnClick(View view) {
-        setWorkings("AC");
+
     }
 
     public void devideOnClick(View view) {
@@ -45,50 +45,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void nineOnClick(View view) {
+        setWorkings("9");
     }
 
     public void fourOnClick(View view) {
+        setWorkings("4");
     }
 
     public void fiveOnClick(View view) {
+        setWorkings("5");
     }
 
     public void sixOnClick(View view) {
+        setWorkings("6");
     }
 
     public void oneOnClick(View view) {
+        setWorkings("1");
     }
 
     public void twoOnClick(View view) {
+        setWorkings("2");
     }
 
     public void threeOnClick(View view) {
+        setWorkings("3");
     }
 
     public void positiveToNegative(View view) {
+
     }
 
     public void zeroOnClick(View view) {
+        setWorkings("0");
     }
 
     public void dotOnClick(View view) {
+        setWorkings(".");
     }
 
     public void backSpaceOnClick(View view) {
     }
 
     public void modularDivisionOnClick(View view) {
+        setWorkings("%");
     }
 
     public void multiplyOnClick(View view) {
+        setWorkings("*");
     }
 
     public void minusOnClick(View view) {
+        setWorkings("-");
     }
 
     public void plusOnclick(View view) {
+        setWorkings("+");
     }
 
     public void equalsOnClick(View view) {
+
     }
 }
