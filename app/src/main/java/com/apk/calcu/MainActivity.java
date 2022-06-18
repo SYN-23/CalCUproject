407f0448c0d68788e54a7064c71bc11f9f136285
MST dAnT3 <avinandangayen013@gmail.com>
2022-06-19 00:57:15 +0530

    created classes

--- app/src/main/java/com/apk/calcu/MainActivity.java
+++ app/src/main/java/com/apk/calcu/MainActivity.java
@@ -13,7 +13,63 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void numberEvent(View view) {
+    public void clearOnClick(View view) {
+    }
 
+    public void devideOnClick(View view) {
+    }
+
+    public void sevenOnClick(View view) {
+    }
+
+    public void eightOnClick(View view) {
+    }
+
+    public void nineOnClick(View view) {
+    }
+
+    public void fourOnClick(View view) {
+    }
+
+    public void fiveOnClick(View view) {
+    }
+
+    public void sixOnClick(View view) {
+    }
+
+    public void oneOnClick(View view) {
+    }
+
+    public void twoOnClick(View view) {
+    }
+
+    public void threeOnClick(View view) {
+    }
+
+    public void positiveToNegative(View view) {
+    }
+
+    public void zeroOnClick(View view) {
+    }
+
+    public void dotOnClick(View view) {
+    }
+
+    public void backSpaceOnClick(View view) {
+    }
+
+    public void modularDivisionOnClick(View view) {
+    }
+
+    public void multiplyOnClick(View view) {
+    }
+
+    public void minusOnClick(View view) {
+    }
+
+    public void plusOnclick(View view) {
+    }
+
+    public void equalsOnClick(View view) {
     }
 }
